9e141be32aab6db596d1cc9e7be4724e9f6a4697
Rayne Flores <rayne.flores@digital-cupcake.com>
2021-06-11 11:43:17 -0400

    1. Mejoras en el users_layout.xml 2. Refactorizacion de los titulos en el Adaptador de Usuarios 3. Eliminacion de Layout innecesario(activity_users2.xml)

--- app/src/main/java/com/ryfsystems/a3dprinter/adapters/UserAdapter.java
+++ app/src/main/java/com/ryfsystems/a3dprinter/adapters/UserAdapter.java
@@ -1,7 +1,11 @@
 package com.ryfsystems.a3dprinter.adapters;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -74,8 +78,12 @@ public class UserAdapter extends RecyclerView.Adapter<UserViewHolder> {
         viewHolder.txtListUserId.setText(userList.get(i).getUId());
         viewHolder.txtListUserName.setText(userList.get(i).getUName());
         viewHolder.txtListUserPassword.setText(userList.get(i).getUPassword());
-        viewHolder.txtListUserEmail.setText("Email: " + userList.get(i).getUEmail());
-        viewHolder.txtListUserPhone.setText("Telefono: " + userList.get(i).getUPhone());
+        SpannableString email = new SpannableString("Email: " + userList.get(i).getUEmail());
+        email.setSpan(new StyleSpan(Typeface.BOLD), 0, 5, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        viewHolder.txtListUserEmail.setText(email);
+        SpannableString phone = new SpannableString("Telefono: " + userList.get(i).getUPhone());
+        phone.setSpan(new StyleSpan(Typeface.BOLD), 0, 8, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        viewHolder.txtListUserPhone.setText(phone);
         viewHolder.chkListUserIsAdmin.setChecked(userList.get(i).getUAdmin() == 1L);
     }
 
